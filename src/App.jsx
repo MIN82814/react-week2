0dc219e306a2605e1d70e7f717e8988207aeb823
Min <min82814@gmial.com>
2026-01-06 16:35:52 +0800

    git init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+ 
+<div className="container">
+  <p>week2</p>
+</div>
+   
+  )
+}
+
+export default App
